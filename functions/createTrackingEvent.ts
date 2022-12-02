79b2b50f2b8e1c22a059ccc9cda5d4c4d4459d75
tomfrew <tom@keel.xyz>
2022-12-02 17:13:48 +0000

    Track type

--- functions/createTrackingEvent.ts
+++ functions/createTrackingEvent.ts
@@ -1,39 +1,41 @@
-import { CreateTrackingEvent } from '@teamkeel/sdk'
-import Pusher from 'pusher';
+import { CreateTrackingEvent } from "@teamkeel/sdk";
+import Pusher from "pusher";
 
-export default CreateTrackingEvent(async ({name, metadata, projectId}, api) => {
-  const createResult = await api.models.trackingEvent.create({
-    name: name,
-    projectId: projectId,
-    metadata: metadata,
-  })
+export default CreateTrackingEvent(async (inputs, api) => {
+  const createResult = await api.models.trackingEvent.create(...inputs);
 
   if (createResult.errors && createResult.errors.length) {
     return createResult;
   }
 
-  const createResultObj = createResult.object!
+  const createResultObj = createResult.object!;
 
   const pusher = new Pusher({
     appId: "1518900",
     key: "0605a8744414f200668e",
     secret: "28f38f7691c0c3286005",
     cluster: "eu",
-    useTLS: true
+    useTLS: true,
   });
 
-  const pusherChannel = "proj-" + projectId
-  const pusherEvent = "track"
-  
-  const pushResult = await pusher.trigger(pusherChannel, pusherEvent, createResultObj);
+  const pusherChannel = "proj-" + inputs.projectId;
+  const pusherEvent = "track";
+
+  const pushResult = await pusher.trigger(
+    pusherChannel,
+    pusherEvent,
+    createResultObj
+  );
   if (!pushResult.ok) {
     return {
-      errors: [{
-        message: "push not ok: " + pushResult.statusText,
-        stack: "",
-      }]
-    }
+      errors: [
+        {
+          message: "push not ok: " + pushResult.statusText,
+          stack: "",
+        },
+      ],
+    };
   }
 
   return createResult;
-})
+});
