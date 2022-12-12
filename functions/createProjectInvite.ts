d3fc88e864443badfd15ebb83a12a60ce6a0107d
Hugo Sousa <gi.ciberon@gmail.com>
2022-12-12 10:29:25 +0100

    Implement invites (minus email content

--- functions/createProjectInvite.ts
+++ functions/createProjectInvite.ts
@@ -1,4 +1,8 @@
 import { CreateProjectInvite } from '@teamkeel/sdk'
+import fetch from "node-fetch";
+
+// should be a secret
+const POSTMARK_SERVER_TOKEN = "371a6b7b-5973-41e6-966c-c78929b0d9be"
 
 export default CreateProjectInvite(async (inputs, api) => {
   // we disregard the invite expiry that was sent as an input
@@ -8,7 +12,23 @@ export default CreateProjectInvite(async (inputs, api) => {
 
   const createResult = await api.models.projectInvite.create(inputs);
 
-  //TODO: sent email
+  await fetch("https://api.postmarkapp.com/email", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json",
+      "X-Postmark-Server-Token": POSTMARK_SERVER_TOKEN,
+    },
+    body: JSON.stringify({
+      "From": "chirpy@keel.xyz",
+      "To": inputs.email,
+      "Subject": "You've been invited to a project",
+      "HtmlBody": "<strong>Hello</strong> dear Chirper, you've been invited to a project",
+      "MessageStream": "chirpy"
+    }),
+}).catch(reason => {
+    console.error("Couldn't track event of page being opened due to ", reason);
+});
 
   return createResult;
 })
