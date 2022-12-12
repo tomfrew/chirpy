e46a87481ff949a08d5918a2c25a15f9e5d353a5
Hugo Sousa <gi.ciberon@gmail.com>
2022-12-12 11:49:45 +0100

    Invite expiry

--- functions/acceptInvite.ts
+++ functions/acceptInvite.ts
@@ -2,13 +2,39 @@ import { AcceptInvite } from '@teamkeel/sdk'
 
 export default AcceptInvite(async (inputs, api) => {
   // there should only be one but the fields aren't unique, as it's a linking table and only the pair is unique
-  const projectInvitesResult = await api.models.projectInvite.findMany({id: inputs.projectInviteId});
+  const projectInvitesResult = await api.models.projectInvite.findMany({ id: inputs.projectInviteId });
 
   if (projectInvitesResult.errors && projectInvitesResult.errors.length) {
     return { errors: projectInvitesResult.errors };
   }
 
   const projectInvites = projectInvitesResult.collection!;
+
+  if (projectInvites.length !== 1) {
+    return {
+      errors: [
+        {
+          message: "internal server error",
+          stack: "",
+        },
+      ],
+    };
+  }
+
+  const projectInvite = projectInvites[0];
+
+  const inviteHasExpired = projectInvite.expiry < new Date();
+
+  if (inviteHasExpired) {
+    return {
+      errors: [
+        {
+          message: "invite has expired",
+          stack: "",
+        },
+      ],
+    };
+  }
 
 
   const createAccountProjectResult = await api.models.accountProject.create({
@@ -20,13 +46,13 @@ export default AcceptInvite(async (inputs, api) => {
     return { errors: createAccountProjectResult.errors };
   }
 
-  for (const projectInvite of projectInvites) {
-    const deleteResult = await api.models.projectInvite.delete(projectInvite.id);
 
-    if (deleteResult.errors && deleteResult.errors.length) {
-      return deleteResult;
-    }
+  const deleteResult = await api.models.projectInvite.delete(projectInvite.id);
+
+  if (deleteResult.errors && deleteResult.errors.length) {
+    return deleteResult;
   }
+
 
   return createAccountProjectResult;
 })
